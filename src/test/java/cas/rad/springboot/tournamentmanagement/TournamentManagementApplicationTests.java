c902b2ddc91da560eb6f745c68cafe6effd3b0cf
Junior Ouangala <ouangalajunior@MacBook-Air-de-Junior.local>
2024-03-15 18:57:51 +0100

    Tournament Management initial setup

---
+++ src/test/java/cas/rad/springboot/tournamentmanagement/TournamentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cas.rad.springboot.tournamentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
